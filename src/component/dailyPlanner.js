90296d05c6e2dba51fa19ea6659f70e31985a6ee
Gummybearr <ehrudghl123@naver.com>
2020-11-18 15:12:09 +0900

    Add daily planner

---
+++ src/component/dailyPlanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './dailyPlanner.css';
+
+const DailyPlanner = (props) => {
+
+    let addflag = 0;
+    
+    const test = () => {
+        alert('testing');
+    }
+
+    return (
+    <div>testing</div>);
+};
+
+export default DailyPlanner;
--- src/component/weeklyPlanner.js
+++ src/component/weeklyPlanner.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './weeklyPlanner.css';
 
+import DailyPlanner from './dailyPlanner';
+
 const WeeklyPlanner = (props) => {
+
+    let addflag = 0;
+
+    const test = () => {
+        alert('testing');
+    }
+
     return (
     <div className='week-outline'>
         <div className='day-container'>
-            <div className='day-outline'><div className='day-text'>Sun</div></div>
-            <div className='day-outline'><div className='day-text'>Mon</div></div>
-            <div className='day-outline'><div className='day-text'>Tues</div></div>
-            <div className='day-outline'><div className='day-text'>Wed</div></div>
-            <div className='day-outline'><div className='day-text'>Thu</div></div>
-            <div className='day-outline'><div className='day-text'>Fri</div></div>
-            <div className='day-outline-no-border'><div className='day-text'>Sat</div></div>
+            <div className='day-outline'><div className='day-text'>Sun</div><div onClick={test}><DailyPlanner></DailyPlanner></div></div>
+            <div className='day-outline'><div className='day-text'>Mon</div><DailyPlanner></DailyPlanner></div>
+            <div className='day-outline'><div className='day-text'>Tues</div><DailyPlanner></DailyPlanner></div>
+            <div className='day-outline'><div className='day-text'>Wed</div><DailyPlanner></DailyPlanner></div>
+            <div className='day-outline'><div className='day-text'>Thu</div><DailyPlanner></DailyPlanner></div>
+            <div className='day-outline'><div className='day-text'>Fri</div><DailyPlanner></DailyPlanner></div>
+            <div className='day-outline-no-border'><div className='day-text'>Sat</div><DailyPlanner></DailyPlanner></div>
         </div>
     </div>);
 };
